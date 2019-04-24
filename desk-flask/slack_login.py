ae23e38a4250db71501500208d317ae540554839
Stuart Geiger <sgeiger@gmail.com>
2019-04-23 17:13:02 -0700

    Create slack_login.py

---
+++ desk-flask/slack_login.py
@@ -0,0 +1,3 @@
+slack_token = ""  # API Key
+channel = ""      # The API channel ID for the direct message channel between the bot and the authorized sender
+sender_id = ""    # The authorized sender's API user ID (not the username)
